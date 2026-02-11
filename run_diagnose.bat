d6258982cc515c1210a7e35f88d23be32ad72172
kzzz-jpg <211096@stu.cchs.chc.edu.tw>
2026-02-11 22:05:56 +0800

    remove debug

--- run_diagnose.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo [DIAGNOSE] Running python diagnosis...
-if not exist "venv\Scripts\python.exe" (
-    echo VENV Python not found!
-    exit /b 1
-)
-venv\Scripts\python.exe diagnose.py
-pause
